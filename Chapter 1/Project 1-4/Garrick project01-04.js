3162747edd2523827852fe36d208834459936396
Garrick D. Smith <153487653+Ilovetohackandprogram@users.noreply.github.com>
2024-07-22 13:21:58 -0400

    Add files via upload

---
+++ Chapter 1/Project 1-4/Garrick project01-04.js
@@ -0,0 +1,11 @@
+/* Garrick's Hand-on Project 1-4
+      Hands-on Project 1-4
+	  
+	  07-15-2024
+	  
+	  WCCCD
+*/
+
+//define variables for home and work addresses
+let homeStreet = "1 Main St.", homeCity = "Sicilia", homeState = "MA", homeCode = "02103";
+let workStreet = "15 Oak Ln.", workCity = "Central City", workState = "MA", workCode = "02104";
