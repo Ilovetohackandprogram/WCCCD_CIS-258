bb06295d340f82dc2fd9151dd18b16c05ec8e1c2
Garrick D. Smith <153487653+Ilovetohackandprogram@users.noreply.github.com>
2024-07-27 10:36:37 -0400

    Add files via upload

---
+++ Chapter 11/Project 11-2/Garrick's project 11-02.js
@@ -0,0 +1,27 @@
+"use strict";
+/*    Garrick D. Smith
+	  Hands-on Project 11-02
+	   
+	   WCCCD
+	  
+	  07-27-2024
+*/
+
+let postalCode = document.getElementById("postalCode");
+let place = document.getElementById("place");
+let region = document.getElementById("region");
+let country = document.getElementById("country");
+
+postalCode.onblur = function() {
+let codeValue = postalCode.value;
+let countryValue = country.value;
+place.value = "";
+region.value = "";
+    fetch(`http://api.zippopotam.us/${countryValue}/${codeValue}`)
+         .then(response => response.json())
+         .then(json => {
+          place.value = json.places[0]["place name"];
+         region.value = json.places[0]["state abbreviation"]
+    })
+            .catch(error => console.log(error));
+}
