7f9c879ee641837ba6e863d6b749bd8b7d05c2fb
Garrick D. Smith <153487653+Ilovetohackandprogram@users.noreply.github.com>
2024-08-02 21:13:02 -0400

    Add files via upload

---
+++ Chapter 11/Project 11-3/main.js
@@ -0,0 +1,23 @@
+"use strict";
+/*    Garrick D. Smith
+	  08-02-2024
+	  WCCCD
+	  CIS-258
+*/
+
+let orderResult = document.getElementById("orderResult");
+let userIDBox = document.getElementById("userID");
+let pwdBox = document.getElementById("pwd");
+
+
+// Retrieve order history when the View Orders button is clicked
+viewOrders.onclick = function() {
+   
+}
+
+
+// Function to display order history within web tables
+function buildOrderTable(obj) {
+
+}
+document.getElementById (){"No orders found for this user id and password" };
